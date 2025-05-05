c3b32844576592e028d2ae9961beae11e4b21ebd
Kaisaan <34224128+Kaisaan@users.noreply.github.com>
2025-05-04 22:29:44 -0400

    Update building

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 armips patch.asm
-copy DATA.BIN translated/DATA.BIN
 copy "SLPM_663.60" "translated/SLPM_663.60"
+perl "scripts/abcde/abcde.pl" -cm abcde::Atlas "translated/DATA.BIN" "scripts/translated.txt"
 isotool.py -m insert --iso lostkefin.iso -o english.iso --filelist filelist.txt --files translated
 @pause
