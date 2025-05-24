a181f08aaa531adb88ff520554b2cd7e1ed4c6ba
Kaisaan <34224128+Kaisaan@users.noreply.github.com>
2025-05-24 00:45:10 -0400

    Update batch files

--- 2 Patch ISO.bat
+++ 2 Patch ISO.bat
@@ -3,5 +3,6 @@ xcopy extracted translated /E /I /Y
 armips patch.asm
 echo Patching ISO. Please wait.
 perl scripts\abcde\abcde.pl --multi-table-files -cm abcde::Atlas "translated\DATA.BIN" "scripts\translated.txt"
+armips patch.asm
 isotool.py -m insert --iso lostkefin.iso -o "Ys V - Lost Kefin, Kingdom of Sand [English Patched].iso" --filelist filelist.txt --files translated
 pause
