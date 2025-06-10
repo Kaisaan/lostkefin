1ec4950ce31a88d538149c2a775ff5df007ba5be
Kaisaan <34224128+Kaisaan@users.noreply.github.com>
2025-06-10 19:35:34 -0400

    Fix textbox sizing issues

--- patch.asm
+++ patch.asm
@@ -1,12 +1,16 @@
-.erroronwarning off
+.erroronwarning on
 .ps2
 
-.loadtable "kefinold.tbl", "SJIS"
+.loadtable "kefin.tbl", "SJIS"
 
-.open "extracted/SLPM_663.60", "SLPM_663.60", 0x0
+.open "extracted/SLPM_663.60", "translated/SLPM_663.60", 0x0
+
+// Fix halfwidth characters not updating textbox size correctly
+.orga 0x0004CE40	; 43 50 07 00	sra		t2,a3,0x1
+.db 0x03			; change 0x1 to 0x0
 
 .orga 0x0025FD45
-.strn "KaisaanTest", 0x81, 0xA5
+.strn "KaisaanTest"
 
 .orga 0x0025FE70
 .strn "Ys 3 Data found"
@@ -22,5 +26,12 @@
 
 .orga 0x2638E0
 .strn "Dogi"
+
+.close
+
+.open "extracted/DATA.BIN", "translated/DATA.BIN", 0x0
+
+.orga 0x3B9A880
+.db 0xFF
 
 .close
