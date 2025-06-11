e96a1b5b99eb3caa34e0db80be1194aed30f1f41
Etokapa <31154606+Etokapa@users.noreply.github.com>
2025-06-11 16:31:45 -0500

    Add more entries

--- patch.asm
+++ patch.asm
@@ -9,8 +9,8 @@
 .orga 0x0004CE40
 .db 0x03
 
-
 // Bootup Text ------------------------------------------------
+
 .orga 0x0025FD45
 .strn "KaisaanTest"
 
@@ -32,7 +32,7 @@
 .strn "Dogi"
 
 .orga 0x2638E8
-.strn "Niena"
+.strn "Niena", 0x00
 
 .orga 0x2638F0
 .strn "Terra"
@@ -49,6 +49,119 @@
 .orga 0x263920
 .strn "Rizze", 0x00
 
+.orga 0x263928
+.strn "Javil", 0x00, 0x00, 0x00
+
+.orga 0x263938
+.strn "Effey", 0x00
+
+.orga 0x263940
+.strn "King Kefin"
+
+.orga 0x263950
+.strn "Alga", 0x00, 0x00
+
+.orga 0x263958
+.strn "Deios", 0x00, 0x00, 0x00
+
+.orga 0x263968
+.strn "Nottis", 0x00, 0x00
+
+.orga 0x263978
+.strn "Chase", 0x00, 0x00, 0x00
+
+.orga 0x263988
+.strn "Aisha", 0x00, 0x00, 0x00
+
+.orga 0x263998
+.strn "Willy", 0x00, 0x00, 0x00
+
+.orga 0x2639A8
+.strn "Mujaba", 0x00, 0x00, 0x00
+
+.orga 0x2639B8
+.strn "Balk", 0x00, 0x00
+
+.orga 0x2639C0
+.strn "Abyss", 0x00
+
+.orga 0x2639C8
+.strn "Carion", 0x00, 0x00
+
+//Blacksmith
+//.orga 0x2639D8
+//.strn "鍛冶屋"
+
+.orga 0x2639E0
+.strn "Sailor"
+
+.orga 0x2639E8
+.strn "Butler"
+
+.orga 0x2639F0
+.strn "Maid", 0x00, 0x00
+
+//Foot Soldier
+//.orga 0x263
+//.strn "下級兵士"
+
+.orga 0x263A08
+.strn "Soldier"
+
+// TODO?
+//Adventurer A
+//Adventurer B
+//Merchant A
+//Merchant B
+//Kobold
+//Desert Old Woman
+//Desert Old Man
+//Desert Woman
+//Desert Man A
+//Desert Man B
+//Desert Girl
+//Desert Boy
+//Kefin Man A
+//Kefin Man B
+//Kefin Woman A
+//Kefin Woman B
+//Rebel A
+//Rebel B
+//Man A
+//Man B
+//Woman
+//Villager Man A
+//Villager Man B
+//Villager Girl A
+//Villager Girl B
+//Old Woman
+//Old Man
+//Boy A
+//Boy B
+//Girl A
+//Dog
+//Girl B
+//Kidnapped Stein
+//Noties & Deios
+//Beautiful Woman
+//Girl
+//Thug A
+//Thug B
+//Mysterious Voice
+//Mysterious Man
+//Big Man
+//Woman
+
+// Load Menu --------------------------------------------
+
+.orga 0x2648F0
+.strn "Xandria Port", 0x00, 0x00
+
+//.orga 0x260B28
+//.strn "Yes", 0x00
+
+//.orga 0x260B30
+//.strn "No", 0x00, 0x00, 0x00, 0x00
 
 // ------------------------------------------------------------
 
