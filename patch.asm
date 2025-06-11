7d7e5baf79b4c22cc616402869dfe78d05d7f941
Etokapa <31154606+Etokapa@users.noreply.github.com>
2025-06-10 22:01:59 -0500

    Adding a few names.

--- patch.asm
+++ patch.asm
@@ -6,9 +6,11 @@
 .open "extracted/SLPM_663.60", "translated/SLPM_663.60", 0x0
 
 // Fix halfwidth characters not updating textbox size correctly
-.orga 0x0004CE40	; 43 50 07 00	sra		t2,a3,0x1
-.db 0x03			; change 0x1 to 0x0
+.orga 0x0004CE40
+.db 0x03
 
+
+// Bootup Text ------------------------------------------------
 .orga 0x0025FD45
 .strn "KaisaanTest"
 
@@ -21,11 +23,34 @@
 .orga 0x00260B70
 .strn "Ys 3 Data search"
 
+// Names ------------------------------------------------------
+
 .orga 0x2638D8
 .strn "Adol", 0x00, 0x00
 
 .orga 0x2638E0
 .strn "Dogi"
+
+.orga 0x2638E8
+.strn "Niena"
+
+.orga 0x2638F0
+.strn "Terra"
+
+.orga 0x2638F8
+.strn "Massea", 0x00, 0x00
+
+.orga 0x263908
+.strn "Stein", 0x00
+
+.orga 0x263910
+.strn "Doman" , 0x00, 0x00, 0x00
+
+.orga 0x263920
+.strn "Rizze", 0x00
+
+
+// ------------------------------------------------------------
 
 .close
 
