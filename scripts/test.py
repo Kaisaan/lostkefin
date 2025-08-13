e23538969c7bb3738ea77db99bf888d455b48d83
Cameron Schmidt <4950661+cschmidt0121@users.noreply.github.com>
2025-08-12 23:07:00 -0400

    Accidentally commited a test file

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-from isotool import PAD_NONE, rebuild_iso
-from pathlib import Path
-rebuild_iso(
-    Path("lostkefin.iso"),
-    Path("filelist.txt"),
-    Path("translated"),
-    Path("Ys V - Lost Kefin, Kingdom of Sand [English Patched].iso"),
-    PAD_NONE,
-)
