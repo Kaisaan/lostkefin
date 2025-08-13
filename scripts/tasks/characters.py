4f91ebed819929159b68cf9ba3c5f0327319f92b
Cameron Schmidt <4950661+cschmidt0121@users.noreply.github.com>
2025-08-12 23:40:21 -0400

    Update char names

--- scripts/tasks/characters.py
+++ scripts/tasks/characters.py
@@ -10,71 +10,71 @@ CHARACTERS = [
     "Terra",                   # $3
     "Massea",                  # $4
     "Stein",                   # $5
-    "Doman",                   # $6
+    "Dorman",                  # $6
     "Rizze",                   # $7
     "Jabir",                   # $8
-    "Effey",                   # $9
-    "King Kefin",              # $A
+    "Effie",                   # $9
+    "King of Kefin",           # $A
     "Alga",                    # $B
     "Dios",                    # $C
     "Nottis",                  # $D
     "Chase",                   # $E
     "Aisha",                   # $F
     "Willy",                   # $10
-    "Mubaja",                  # $11
-    "Baruq",                   # $12
+    "Muhaba",                  # $11
+    "Balck",                   # $12
     "Abyss",                   # $13
     "Karion",                  # $14
     "Blacksmith",              # $15
     "Sailor",                  # $16
     "Butler",                  # $17
     "Maid",                    # $18
-    "Foot Soldier",            # $19
+    "Lower Ranking Soldier",   # $19
     "Soldier",                 # $1A
     "Adventurer A",            # $1B
     "Adventurer B",            # $1C
     "Merchant A",              # $1D
     "Merchant B",              # $1E
     "Kobold",                  # $1F
-    "Desert Old Woman",        # $20
-    "Desert Old Man",          # $21
-    "Desert Woman",            # $22
+    "Desert Grandma",          # $20
+    "Desert Grandpa",          # $21
+    "Desert Lady",             # $22
     "Desert Man A",            # $23
     "Desert Man B",            # $24
     "Desert Girl",             # $25
     "Desert Boy",              # $26
     "Kefin Man A",             # $27
     "Kefin Man B",             # $28
-    "Kefin Woman A",           # $29
-    "Kefin Woman B",           # $2A
-    "Rebel A",                 # $2B
-    "Rebel B",                 # $2C
-    "Man A",                   # $2D
-    "Man B",                   # $2E
-    "Woman",                   # $2F
-    "Villager Man A",          # $30
-    "Villager Man B",          # $31
-    "Villager Girl A",         # $32
-    "Villager Girl B",         # $33
-    "Old Woman",               # $34
-    "Old Man",                 # $35
+    "Kefin Lady A",            # $29
+    "Kefin Lady B",            # $2A
+    "Resistance A",            # $2B
+    "Resistance B",            # $2C
+    "Grandpa A",               # $2D
+    "Grandpa B",               # $2E
+    "Grandma",                 # $2F
+    "Townsman A",              # $30
+    "Townsman B",              # $31
+    "Townslady A",             # $32
+    "Townslady B",             # $33
+    "Grandma",                 # $34
+    "Grandpa",                 # $35
     "Boy A",                   # $36
     "Boy B",                   # $37
     "Girl A",                  # $38
     "Dog",                     # $39
     "Girl B",                  # $3A
     "Kidnapped Stein",         # $3B
-    "Noties & Deios",          # $3C
+    "Nottis & Dios",           # $3C
     None,                      # $3D (missing in table)
     None,                      # $3E (missing in table)
     None,                      # $3F (missing in table)
     "Beautiful Woman",         # $40
-    "Girl",                    # $41
-    "Thug A",                  # $42
-    "Thug B",                  # $43
+    "Young Girl",              # $41
+    "Ruffian A",               # $42
+    "Ruffian B",               # $43
     "Mysterious Voice",        # $44
     "Mysterious Man",          # $45
-    "Big Man",                 # $46
+    "Burly Man",               # $46
     "Woman",                   # $47
 ]
 
